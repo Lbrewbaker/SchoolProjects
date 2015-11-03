a5c051307bf68c2a0d82d28a163e8e6a3c4b1aa7
Luke Brewbaker <hybridsquirrel@gmail.com>
2015-11-02 16:13:59 -1000

    final version
    for submission.  fixed the button location

--- Assignments/DOM and Javascript/script.js
+++ Assignments/DOM and Javascript/script.js
@@ -1,75 +1,58 @@
-//Building the table
-function buildTable(){
+function buildTable() {
 	
-	var newTable = document.createElement("table");
+   var newTable = document.createElement("table");
+
+   
+   //Var I = Rows
+   //Var J = columns
+    for (var i = 0; i < 4; i++) {
+        var tr = document.createElement('tr');
+
+        for (var j = 0; j < 4; j++) {
+            var td = document.createElement('td');
+			
+			if (i == 0 && j != 4){
+				td.appendChild(document.createTextNode("Header "))
+				td.appendChild(document.createTextNode((j+1)))
+				tr.appendChild(td)
+
+			}
+			
+			else {
+                td.appendChild(document.createTextNode(i))
+				td.appendChild(document.createTextNode(", "))
+				td.appendChild(document.createTextNode((j+1)))
+				tr.appendChild(td)
+            }
+			
+
+        }
+
+       newTable.appendChild(tr);
+    }
 	
-	for(var i = 0; i < 4; i++){
-		var tbRow = document.createElement("tr");
-		var tbData1 = document.createElement("td");
-		var tbData2 = document.createElement("td");
-		var tbData3 = document.createElement("td");
-		var tbData4 = document.createElement("td");
-
-		if(i == 0){
-			var textNd1 = document.createTextNode("Header 1");
-			var textNd2 = document.createTextNode("Header 2");
-			var textNd3 = document.createTextNode("Header 3");
-			var textNd4 = document.createTextNode("Header 4");
-		}
-		
-		else if(i == 1){
-			var textNd1 = document.createTextNode("1, 1");
-			var textNd2 = document.createTextNode("2, 1");
-			var textNd3 = document.createTextNode("3, 1");
-			var textNd4 = document.createTextNode("4, 1");
-		}
-		
-		else if(i == 2){
-			var textNd1 = document.createTextNode("1, 2");
-			var textNd2 = document.createTextNode("2, 2");
-			var textNd3 = document.createTextNode("3, 2");
-			var textNd4 = document.createTextNode("4, 2");
-		}
-		
-		else if(i == 3){
-			var textNd1 = document.createTextNode("1, 3");
-			var textNd2 = document.createTextNode("2, 3");
-			var textNd3 = document.createTextNode("3, 3");
-			var textNd4 = document.createTextNode("4, 3");
-		}	
-		
-		tbData1.appendChild(textNd1);
-		tbData2.appendChild(textNd2);
-		tbData3.appendChild(textNd3);
-		tbData4.appendChild(textNd4);
-
-		tbRow.appendChild(tbData1);
-		tbRow.appendChild(tbData2);
-		tbRow.appendChild(tbData3);
-		tbRow.appendChild(tbData4);
-
-		newTable.appendChild(tbRow);
-	}
     document.getElementById("tableHere").appendChild(newTable);
 	newTable.setAttribute("border", 3);
-	return newTable;
+	newTable.style.width = '50%';
+    return newTable;
 }
 
 function initTable(table){
-	table.children[1].children[0].style.borderStyle = "solid";
-	table.children[1].children[0].style.borderWidth = "thick";
 	return table.children[1].children[0];
 }
 
-
 var table = buildTable();
 var currentCell = initTable(table);
+
 
 //change cell border to thin
 function changeCell(cell){
     	cell.style.borderWidth = "thin";
 		cell.style.backgroundColor = "yellow";
 }
+
+
+
 
 //                   Button section                       //
 
@@ -88,22 +71,18 @@ function goDown(table, cell){
 	}
 	
 	else if(cell == cell.parentNode.children[0]){
-		console.log(cell.parentNode.children[0]);
 		newCell = cell.parentNode.nextElementSibling.children[0];
 	}
 	
 	else if(cell == cell.parentNode.children[1]){
-		console.log(cell.parentNode.children[1]);
 		newCell = cell.parentNode.nextElementSibling.children[1];
 	}
 	
 	else if(cell == cell.parentNode.children[2]){
-		console.log(cell.parentNode.children[2]);
 		newCell = cell.parentNode.nextElementSibling.children[2];
 	}
 	
 	else if(cell == cell.parentNode.children[3]){
-		console.log(cell.parentNode.children[3]);
 		newCell = cell.parentNode.nextElementSibling.children[3];
 	}
 	
@@ -129,22 +108,18 @@ function goUp(table, cell){
 	}
 	
 	else if(cell == cell.parentNode.children[3]){
-		console.log(cell.parentNode.children[3]);
 		newCell = cell.parentNode.previousElementSibling.children[3];
 	}
 
 	else if(cell == cell.parentNode.children[2]){
-		console.log(cell.parentNode.children[2]);
 		newCell = cell.parentNode.previousElementSibling.children[2];
 	}
 	
 	else if(cell == cell.parentNode.children[1]){
-		console.log(cell.parentNode.children[1]);
 		newCell = cell.parentNode.previousElementSibling.children[1];
 	}
 	
 	else if(cell == cell.parentNode.children[0]){
-		console.log(cell.parentNode.children[0]);
 		newCell = cell.parentNode.previousElementSibling.children[0];
 	}
 
@@ -168,17 +143,14 @@ function goRight(table, cell){
 	}
 		
 	if(cell == cell.parentNode.children[0]){
-        console.log(cell);
         newCell = cell.parentNode.children[1];
 	}
 	
 	else if(cell == cell.parentNode.children[1]){
-		console.log(cell);
 		newCell = cell.parentNode.children[2];
 	}
 	
 	else if(cell == cell.parentNode.children[2]){
-		console.log(cell);
 		newCell = cell.parentNode.children[3];
 	}
 
@@ -206,17 +178,14 @@ function goLeft(table, cell){
 	}
 	
 	if(cell == cell.parentNode.children[1]){
-        console.log(cell);
         newCell = cell.parentNode.children[0];
 	}
 	
 	else if(cell == cell.parentNode.children[2]){
-		console.log(cell);
 		newCell = cell.parentNode.children[1];
 	}
 	
 	else if(cell == cell.parentNode.children[3]){
-		console.log(cell);
 		newCell = cell.parentNode.children[2];
 	}
 
@@ -230,4 +199,3 @@ function goLeft(table, cell){
 document.getElementById("left").addEventListener("click", function(){
 	currentCell = goLeft(table, currentCell);
 });
-
